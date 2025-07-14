13b4fc5ea7a6bc3f98c24b64cf1d9c57c51976ce
Pmantheballer <plindsley3@gatech.edu>
2025-07-13 23:02:32 -0400

    Add files via upload

---
+++ WebDevLab01/Home_Page.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("Palmer Lindsley")
+st.title("CS1301")
+
+st.write("**Page descriptions**")
+st.write("**Portfolio**:Portfolio page with who I am, my skills, education etc...")
+st.write("**DataPage**: dashboard displaying data")
---
+++ WebDevLab01/info.py
@@ -0,0 +1,72 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/Palmer.Webp"
+about_me = "I'm Palmer Lindsley. I go to Georgia Tech. I am a freshman ignite attendee. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/palmer-lindsley-6b2ab72a5/"
+my_github_url = "https://github.com/Pmantheballer?tab=projects"
+my_email_address = "Plindsley3@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Working on acheiving my Masters of Business With concentration in Finance',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2029',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "INTA1200"], 
+    "names":["Intro to CS", "International Affairs"], 
+    "semester_taken":["1st", "1st",],
+    "skills":["Analyze and learn python", "Explore Georgia and US history and Foreign affairs"],
+    }
+experience_data = {
+   "Golf Course Worker": (["- Greeted and served golfers before/after round"], "images/golf.jpg"),
+    "Florida Senate Volunteer":(["Helped carry messages to and from senators in session"],"images/Floridasen.png"),
+    "Food bank volunteering":(["- Volunteered at local foodbank for 3+ yrs"],"images/Food.jpg"),
+}
+
+projects_data = {
+    "Coding Website": "Created amazing coding website",
+}
+
+programming_data = {
+    "Python": 50,
+    "Java": 0,
+    "C": 70,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "No knowledge",
+    "Spanish": "Speak frequently",
+}
+leadership_data = {
+    "President of SGA ": (["- Served as SGA president Freshman,Sophomore years"],"images/SGA.jpg"),
+
+}
+activity_data={
+    "Coached Middle school golf": ["- Served as assistant and head coach for Destin Middle School", ]
+}
